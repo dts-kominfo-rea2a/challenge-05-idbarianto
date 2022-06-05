44bb3a73a57dfded9a74f965391d130347d6ca60
Sugeng Arif Barianto <id.barianto@gmail.com>
2022-06-05 20:39:57 +0700

    feat: kuis

--- index.js
+++ index.js
@@ -6,20 +6,40 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+// const sorter = null;
+const sorter = (arrData, fnCallback) => {
+
+  let sortedArray = fnCallback(arrData);
+
+  let dataSorted = [];
+  for (let index = 0; index < sortedArray.length; index++) {
+    dataSorted.push(index + 1 + ". " + sortedArray[index]);
+  }
+
+  return dataSorted;
+
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+// const sortAscending = null;
+const sortAscending = (arrData) => {
+  return arrData.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+// const sortDescending = null;
+const sortDescending = (arrData) => {
+  return arrData.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
+
   console.log(sorter?.(names, sortAscending)?.join("\n"));
   console.log(sorter?.(names, sortDescending)?.join("\n"));
+
 })();
 
 module.exports = {
